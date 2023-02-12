67a17095040ec10479141eb9489f7217a49435d8
Ji Xudong <jixudong99@163.com>
2023-02-12 18:50:47 +0800

    vuex配置

---
+++ src/store/state/num.state.js
@@ -0,0 +1,33 @@
+
+
+export default {
+    namespaced: true,
+    state: {
+        count: 1
+    },
+    //计算state获取对应的值
+    getters: {
+        countStatus(state) {
+            return state.count <= 1
+        }
+    },
+    //更新状态的方法 唯一方法更新state, commit mutations
+    mutations: {
+        setCount(state, num) {
+            state.count = num
+        }
+    },
+    //可以异步操作，可以返回promise,更改数据还是传到mutations中去修改
+    actions: {
+        setCountPromise(context, num) {
+            return new Promise((resolve, reject) => {
+                if (num > 100) {
+                    reject('值不能大于100')
+                } else {
+                    context.commit("setCount", num)
+                    resolve()
+                }
+            })
+        }
+    },
+}
